a7fa730922f9235a48f5b2debd07bfa7441ba064
avir123456 <codingaldemo9@gmail.com>
2025-05-18 20:35:00 +0530

    avir

---
+++ script.js
@@ -0,0 +1,13 @@
+ var index = 0;
+ function carousel(){
+    var i;
+    var x = document.getElementsByClassName("myslides")
+ for (i =0; i<x.length, i++){
+    x[i].style.display ="none"
+ }
+index++;
+if(index >x.length){index = 1;}
+x[x-1].style.display = "block";
+setTimeout(carousel,2000)
+ }
+ carousel()
